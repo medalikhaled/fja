71affaaa571ad76c7ec14a2e94c3da301077ace9
Med Ali Khaled <medalikhaled331@gmail.com>
2024-05-12 23:01:13 +0100

    feat: add auth page functionality

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Dashboard } from "~/components/layouts/Dashboard";
+
+function DashboardPage() {
+  return <Dashboard>Hello</Dashboard>;
+}
+
+export default DashboardPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+"use client";
 import Link from "next/link";
 import { Dashboard } from "~/components/layouts/Dashboard";
+import { SessionProvider, useSession } from "next-auth/react";
 
 export default function HomePage() {
   return (
-    <Dashboard>
-      <div>Hello</div>
-    </Dashboard>
+    <SessionProvider>
+      <Index />
+    </SessionProvider>
   );
+}
+
+function Index() {
+  const { data: session, status } = useSession();
+
+  if (status === "authenticated") {
+    return <p>Signed in as {session.user.email}</p>;
+  }
+
+  return <Link href="/api/auth/signin">Sign in</Link>;
 }
