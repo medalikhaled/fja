7d3c281057ae26c080708f688ddd9d6cad2af28a
Med Ali Khaled <medalikhaled331@gmail.com>
2024-05-12 23:24:18 +0100

    readme changes, misc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,32 @@ function Index() {
   const { data: session, status } = useSession();
 
   if (status === "authenticated") {
-    return <p>Signed in as {session.user.email}</p>;
+    return (
+      <main>
+        <p>Signed in as {session.user.email}</p>
+        <Link href="/dashboard">Dashboard</Link>
+      </main>
+    );
   }
 
   return <Link href="/api/auth/signin">Sign in</Link>;
 }
+/**
+ * deployment, ci
+ * local testing docker compose file for db
+ * add loading state
+ * tranistion between pages
+ * login form
+ * more login options
+ * role based auth
+ * pre registered email list
+ * middleware for authorization (protected routes)
+ * zustand/jotai state management for dashboard (naviation, subnavigation)
+ * landing page UI
+ * add custom FJA identity to the Dashboard design
+ * animate FJA Logo (rive)
+ * Implement Email Client UI to send emails (resend, react email)
+ * professional email & domain name
+ * e2e testing
+ * add plausible
+ */
